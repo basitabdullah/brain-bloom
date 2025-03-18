0d65f3764beb7042b986e15d380e1c8400c74f4b
Basit <basitabdullah1122@gmail.com>
2025-03-18 15:32:22 +0530

    Added Some Fixes

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 
 const axiosInstance = axios.create({
-    baseURL : import.meta.env.VITE_REACT_APP_BACKEND_URL,
+    baseURL : "https://brain-bloom-backned.vercel.app/api",
     withCredentials: true,
     headers: {
         "Content-Type": "application/json"
